78266d67e9cc8e6651f544aec93c7d2254067a4a
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2007-12-31 19:24:33 +0000

    Initial import.

---
+++ sdf-eclipse-installer/src/org/syntax_definition/sdf/Activator.java
@@ -0,0 +1,262 @@
+package org.syntax_definition.sdf;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+/**
+ * This activator lazily installs the SDF tools inside of the bundle of
+ * this plugin.
+ */
+public class Activator extends AbstractUIPlugin {
+
+	private static final String INSTALL_FAILED = "Installation of SDF tools failed";
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "sdf-eclipse-installer";
+
+	// The shared instance
+	private static Activator plugin;
+
+	private static final String installPrefix = "install";
+
+	private static final String windowsPrefix = installPrefix;
+
+	private static final String linuxPrefix = installPrefix;
+
+	private static final String distributions = "distributions";
+
+	private static final String linuxInstaller = distributions + File.separator
+			+ "linux.bin.sh";
+
+	private static final String windowsInstaller = distributions
+			+ File.separator + "windows.zip";
+
+	private BundleContext context;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
+	
+	public File getBinaryPrefix() throws IOException {
+		String os = Platform.getOS();
+		if (os.equals(Platform.OS_LINUX)) {
+		  return getFile(context.getBundle(), linuxPrefix + File.separator + "bin");
+		} else if (os.equals(Platform.OS_WIN32)) {
+		  return getFile(context.getBundle(), windowsPrefix  + File.separator + "bin");	
+		} else {
+			return null;
+		}
+	}
+	
+	public String getBinaryPostfix() {
+		String os = Platform.getOS();
+		if (os.equals(Platform.OS_LINUX)) {
+		  return "";
+		} else if (os.equals(Platform.OS_WIN32)) {
+		  return ".exe";	
+		} else {
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		this.context = context;
+		installBinariesIfNeeded(context);
+	}
+
+	/**
+	 * The idea of this method is to select the proper zip file, depending on
+	 * the current platform, unzip it and run some configuration. If the
+	 * binaries have been installed before, this method will do nothing. The
+	 * installation procedure is different for each platform.
+	 * 
+	 * @throws BundleException
+	 * @throws InterruptedException
+	 * 
+	 */
+	private void installBinariesIfNeeded(BundleContext context)
+			throws BundleException, InterruptedException {
+		String arch = Platform.getOSArch();
+
+		if (arch.equals(Platform.ARCH_X86)) {
+			String os = Platform.getOS();
+			if (os.equals(Platform.OS_LINUX)) {
+				installLinuxBinariesIfNeeded(context);
+			} else if (os.equals(Platform.OS_WIN32)) {
+				installWindowsBinariesIfNeeded(context);
+			} else {
+				throw new BundleException(INSTALL_FAILED);
+			}
+		} else {
+			throw new BundleException(INSTALL_FAILED);
+		}
+	}
+
+	private void installWindowsBinariesIfNeeded(BundleContext context) throws BundleException {
+		if (!windowsWasPreviouslyInstalled(context)) {
+			try {
+				File installer = getFile(context.getBundle(), windowsInstaller);
+				File prefix = getFile(context.getBundle(), windowsPrefix);
+				Unzipper.unzip(prefix.getAbsolutePath(), installer.getAbsolutePath());
+				
+			} catch (IOException e) {
+				throw new BundleException(INSTALL_FAILED, e);
+			} 
+		}
+	}
+
+	private boolean windowsWasPreviouslyInstalled(BundleContext context) {
+		return context.getBundle().getResource(
+				windowsPrefix + File.separator + "bin") != null;
+	}
+
+	/**
+	 * Runs the binary installer script for linux providing it with the
+	 * appropriate parameters.
+	 * 
+	 * TODO this method is not robust for all contingencies that may occur while
+	 * running the script.
+	 * 
+	 * @param context
+	 * @throws BundleException
+	 * @throws InterruptedException
+	 */
+	private void installLinuxBinariesIfNeeded(BundleContext context)
+			throws BundleException, InterruptedException {
+		if (!linuxWasPreviouslyInstalled(context)) {
+			try {
+				File installer = getFile(context.getBundle(), linuxInstaller);
+
+				File prefix = getFile(context.getBundle(), linuxPrefix);
+				Process installScript = Runtime.getRuntime().exec(
+						installer.getAbsolutePath());
+
+				OutputStream out = new BufferedOutputStream(installScript
+						.getOutputStream());
+
+				// where to install
+				out.write((prefix.getAbsolutePath() + "\n").getBytes());
+
+				// absolute path of gcc
+				out.write("\n".getBytes());
+
+				// absolute path of java
+				out.write("\n".getBytes());
+
+				// absolute path of dot
+				out.write("\n".getBytes());
+				
+				// yes, overwrite
+				out.write("yes\n".getBytes());
+
+				// make the process receive everything
+				out.close();
+
+				installScript.waitFor();
+
+				if (installScript.exitValue() != 0) {
+					throw new BundleException(INSTALL_FAILED);
+				}
+
+			} catch (IOException e) {
+				throw new BundleException(INSTALL_FAILED, e);
+			}
+		}
+	}
+
+	private File getFile(Bundle bundle, String resourcePath) throws IOException {
+		URL url = bundle.getResource(resourcePath);
+
+		return new File(FileLocator.toFileURL(url).getPath());
+	}
+
+	private boolean linuxWasPreviouslyInstalled(BundleContext context) {
+		return context.getBundle().getResource(
+				linuxPrefix + File.separator + "bin") != null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getInstance() {
+		return plugin;
+	}
+
+	private static class Unzipper {
+		public static void pipe(InputStream in, OutputStream out)
+				throws IOException {
+			byte[] buffer = new byte[1024];
+			int len;
+
+			while ((len = in.read(buffer)) >= 0) {
+				out.write(buffer, 0, len);
+			}
+
+			in.close();
+			out.close();
+		}
+
+		public static void unzip(String where, String zip) throws BundleException {
+			Enumeration entries;
+			ZipFile zipFile;
+
+			try {
+				zipFile = new ZipFile(zip);
+				entries = zipFile.entries();
+
+				while (entries.hasMoreElements()) {
+					ZipEntry entry = (ZipEntry) entries.nextElement();
+					String relative = entry.getName();
+					String absolute = where + File.separator + relative;
+					File file = new File(absolute);
+
+					if (entry.isDirectory()) {
+						if (!file.mkdir()) {
+							throw new BundleException(INSTALL_FAILED);
+						}
+					} else {
+						InputStream input = zipFile.getInputStream(entry);
+						FileOutputStream output = new FileOutputStream(file
+								.getAbsolutePath());
+						pipe(input,	new BufferedOutputStream(output));
+					}
+				}
+
+				zipFile.close();
+			} catch (IOException e) {
+				throw new BundleException(INSTALL_FAILED, e);
+			}
+		}
+	}
+}
---
+++ sdf-eclipse-installer/src/org/syntax_definition/sdf/Tools.java
@@ -0,0 +1,181 @@
+package org.syntax_definition.sdf;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class Tools {
+	private static Activator a = Activator.getInstance();
+
+	private static String[] environment;
+
+	/**
+	 * Execute a command, but first set up the environment and prefix the
+	 * command with the absolute path to the installation of the
+	 * Meta-Environment
+	 * 
+	 * @param commandline
+	 * @return
+	 * @throws IOException
+	 */
+	static private Process exec(String commandline) throws IOException {
+		initializeSearchPath();
+		String command = resolve(commandline);
+		return Runtime.getRuntime().exec(command, environment);
+	}
+	
+	static private String resolve(String commandline) throws IOException {
+		int beyondCommand = commandline.indexOf(' ');
+		
+		if (beyondCommand == -1) {
+			return a.getBinaryPrefix() + File.separator + commandline + a.getBinaryPostfix();
+		}
+		else {
+			String command = commandline.substring(0, beyondCommand);
+			String args = commandline.substring(beyondCommand, commandline.length());
+			return a.getBinaryPrefix() + File.separator + command + a.getBinaryPostfix() + " " + args;
+		}
+	}
+
+	/**
+	 * Convert a string to an input stream for writing to a process
+	 * 
+	 * @param input
+	 * @return
+	 */
+	static public InputStream cat(String input) {
+		return new  ByteArrayInputStream(input.getBytes());
+	}
+
+	/**
+	 * Convert an input stream to a string for reading from a process
+	 * 
+	 * @param input
+	 * @return
+	 * @throws IOException
+	 */
+	static public String uncat(InputStream input) throws IOException {
+		BufferedReader reader = null;
+		String enc = System.getProperty("file.encoding");
+
+		try {
+			StringBuffer buffer = new StringBuffer();
+			char[] part = new char[2048];
+			int read = 0;
+			reader = new BufferedReader(new InputStreamReader(input, enc));
+
+			while ((read = reader.read(part)) != -1)
+				buffer.append(part, 0, read);
+
+			return buffer.toString();
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException ex) {
+					// silently ignored
+				}
+			}
+		}
+	}
+
+	/**
+	 * Execute a command.
+	 * 
+	 * @param command
+	 *            command to run. should start with non-whitespace
+	 * @param input
+	 *            input stream to write to the stdin of the command
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	static public InputStream exec(String command, InputStream input) throws IOException, InterruptedException {
+		return pipeline(new String[] { command }, input);
+	}
+	
+	/**
+	 * This method schedules a list of processes, and connects their standard
+	 * input and standard output in the appropriate way. The user should supply
+	 * an input stream, and gets an inputstream back.
+	 * 
+	 * Note that the commands should not start with spaces, just the
+	 * Meta-Environment command name, and that a .exe suffix is not required.
+	 * 
+	 * @param commands
+	 * @param input
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	static public InputStream pipeline(String[] commands, InputStream input)
+			throws IOException, InterruptedException {
+		Process[] procs = new Process[commands.length];
+
+		for (int i = 0; i < commands.length; i++) {
+			procs[i] = exec(commands[i]);
+		}
+
+		pipe(commands[0], input, procs[0].getOutputStream());
+		
+		for (int i = 1; i < commands.length; i++) {
+			pipe(commands[i], procs[i-1].getInputStream(), procs[i].getOutputStream());
+		}
+		
+		for (int i = 0; i < commands.length; i++) {
+			procs[i].waitFor();
+			if (procs[i].exitValue() != 0) {
+				throw new IOException("Command failed (" + procs[i].exitValue() + "):" + commands[i]);
+			}
+		}
+		
+		return procs[commands.length - 1].getInputStream();
+	}
+
+	private static void initializeSearchPath() {
+		if (environment == null) {
+			try {
+				environment = new String[] { "PATH="
+						+ a.getBinaryPrefix().getAbsolutePath() };
+			} catch (IOException e) {
+				// This can not happen because Activator.getInstance() would
+				// have failed much earlier.
+			}
+		}
+	}
+
+	/**
+	 * A helper method that pipes an inputstream to an output stream using a
+	 * small intermediate buffer. The pipe is executes asynchronously by 
+	 * starting a small worker thread. This is necessary to prevent deadlocks.
+	 * 
+	 * @param in
+	 * @param out
+	 * @throws IOException
+	 */
+	static private void pipe(String label, final InputStream in, final OutputStream out)
+			throws IOException {
+		Runnable worker = new Runnable() {
+			public void run() {
+				try {
+					byte[] buffer = new byte[8192];
+					int count;
+					while ((count = in.read(buffer)) >= 0) {
+						out.write(buffer, 0, count);
+					}
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		new Thread(worker, "pipe: " + label).start();
+	}
+}
