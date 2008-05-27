4ee72de1de009c44521abb554f0f8ef5a86eb1a3
Magiel Bruntink <magiel.bruntink@gmail.com>
2008-05-27 14:30:27 +0000

    LanguageActions is a class that will be called as target of the EditorActionsContribution extension point. It will use LanguageActionsTool to post the relevant action messages to the toolbus.
    LanguageActionsTool is a singleton tool that will post messages to the
    toolbus.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/Activator.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/Activator.java
@@ -2,6 +2,7 @@ package org.meta_environment.eclipse;
 
 import org.eclipse.imp.runtime.PluginBase;
 import org.eclipse.ui.IStartup;
+import org.meta_environment.eclipse.actions.LanguageActionsTool;
 import org.meta_environment.eclipse.errors.ErrorViewer;
 import org.meta_environment.eclipse.files.Resources;
 
@@ -9,6 +10,7 @@ public class Activator extends PluginBase implements IStartup {
 	public static final String PLUGIN_ID = "org.meta_environment";
 
 	private static Activator plugin;
+	private LanguageActionsTool theLanguageActionsTool = null;
 
 	public Activator() {
 		plugin = this;
@@ -21,9 +23,14 @@ public class Activator extends PluginBase implements IStartup {
 	public void earlyStartup() {
 		new ErrorViewer();
 		new Resources();
+		theLanguageActionsTool = new LanguageActionsTool();
 	}
 
 	public String getID() {
 		return PLUGIN_ID;
+	}
+
+	public LanguageActionsTool getTheLanguageActionsTool() {
+		return theLanguageActionsTool;
 	}
 }
