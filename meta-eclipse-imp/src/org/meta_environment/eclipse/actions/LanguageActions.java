7c11ca3b089263d749b2d02d0b50058a0f8b098f
Magiel Bruntink <magiel.bruntink@gmail.com>
2008-05-27 14:30:28 +0000

    LanguageActions is a class that will be called as target of the EditorActionsContribution extension point. It will use LanguageActionsTool to post the relevant action messages to the toolbus.
    LanguageActionsTool is a singleton tool that will post messages to the
    toolbus.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/actions/LanguageActions.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/actions/LanguageActions.java
@@ -1,55 +1,20 @@
 package org.meta_environment.eclipse.actions;
 
-import java.util.Vector;
-
-import nl.cwi.sen1.configapi.Factory;
-import nl.cwi.sen1.configapi.types.ActionDescription;
-import nl.cwi.sen1.configapi.types.Event;
-
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.services.ILanguageActionsContributor;
-import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
-import org.meta_environment.eclipse.Tool;
 
-import aterm.ATermList;
-import aterm.pure.PureFactory;
-
-public class LanguageActions extends Tool implements
-		ILanguageActionsContributor {
-
-	private Factory configFactory;
-
-	public LanguageActions() {
-		super("language-actions");
-		System.out.println("LanguageActions constructor called.");
-		this.configFactory = Factory.getInstance(new PureFactory());
-	}
+public class LanguageActions implements ILanguageActionsContributor {
 
 	public IAction[] getEditorActions(UniversalEditor editor) {
-
-		System.out.println("attempting to get actions for syntax-editor.");
-		ATermList actionList = (ATermList) sendRequest(factory
-				.make("get-actions(syntax-editor)"));
-		Vector actions = new Vector();
-
-		while (!actionList.isEmpty()) {
-
-			ActionDescription desc = configFactory
-					.ActionDescriptionFromTerm(actionList.getFirst());
-			Event action = desc.getEvent();
-
-			if (action.isMenu()) {
-				actions.add(new Action(action.getName()) {
-					public void run() {
-						System.out.println("menu activated");
-					}
-				});
-
-			}
-			actionList = actionList.getNext();
-		}
-
-		return (Action[]) actions.toArray();
+		
+		
+		// TODO: get configured actions for current editor from plugin.xml
+		
+		// TODO: create an Action for each configured action  
+		
+		
+		return null;
 	}
+
 }
