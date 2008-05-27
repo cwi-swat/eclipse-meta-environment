95784987ecda6f1f25ec86ec51a8afde7d5aacf1
Magiel Bruntink <magiel.bruntink@gmail.com>
2008-05-27 14:30:29 +0000

    LanguageActions is a class that will be called as target of the EditorActionsContribution extension point. It will use LanguageActionsTool to post the relevant action messages to the toolbus.
    LanguageActionsTool is a singleton tool that will post messages to the
    toolbus.

---
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/actions/LanguageActionsTool.java
@@ -0,0 +1,12 @@
+package org.meta_environment.eclipse.actions;
+
+import org.meta_environment.eclipse.Tool;
+
+public class LanguageActionsTool extends Tool {
+
+	public LanguageActionsTool() {
+		super("language-actions");
+		System.out.println("LanguageActions constructor called.");
+	}
+
+}
