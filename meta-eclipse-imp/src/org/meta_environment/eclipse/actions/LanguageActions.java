fe19196be2863d7a0d6c39febb7bbd33c440aacc
Magiel Bruntink <magiel.bruntink@gmail.com>
2008-05-29 10:36:21 +0000

    Using Lists instead of Sets menu actions stay in their specified order. The toolbus now also gets the language for which the action needs to performed.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/actions/LanguageActions.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/actions/LanguageActions.java
@@ -1,7 +1,7 @@
 package org.meta_environment.eclipse.actions;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
@@ -20,8 +20,8 @@ public class LanguageActions implements ILanguageActionsContributor {
 
 	public IAction[] getEditorActions(UniversalEditor editor) {
 
-		Set<Action> actions = new HashSet<Action>();
-		// TODO: Request getter for UniversalEditor.fLanguage
+		List<Action> actions = new ArrayList<Action>();
+		// TODO: Bugzilla: Request getter for UniversalEditor.fLanguage
 		final Language language = editor.fLanguage;
 		// TODO: Move extensionPointId to a sensible place
 		final String extensionPointId = "org.meta_environment.eclipse.actions.EditorMenus";
@@ -29,39 +29,22 @@ public class LanguageActions implements ILanguageActionsContributor {
 		IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
 				.getExtensionPoint(extensionPointId);
 
-		if (extensionPoint == null) {
-			System.err.println("LanguageActions: Could not find extension point: "
-					+ extensionPointId);
-		} else {
-			Set<IConfigurationElement> extensions = getExtensionsForLanguage(
+		if (extensionPoint != null) {
+			List<IConfigurationElement> extensions = getExtensionsForLanguage(
 					extensionPoint, language);
 			if (extensions.size() != 0) {
 				for (IConfigurationElement ext : extensions) {
 					actions.addAll(createActionsForEditorMenu(ext, editor));
 				}
-			} else {
-				System.err
-						.println("LanguageActions: Could not find any extensions for extension point: "
-								+ extensionPointId
-								+ " for language "
-								+ language.getName());
 			}
 		}
 
-		// Convert Actions set to array manually (toArray does not seem to work).
-		int length = actions.size();
-		IAction[] actions_array = new IAction[length];
-		int i = 0;
-		for (Action a : actions) {
-			actions_array[i++] = a;
-		}
-
-		return actions_array;
+		return actions.toArray(new Action[0]);
 	}
 
-	private Set<Action> createActionsForEditorMenu(
+	private List<Action> createActionsForEditorMenu(
 			IConfigurationElement editorMenu, final UniversalEditor editor) {
-		Set<Action> actions = new HashSet<Action>();
+		List<Action> actions = new ArrayList<Action>();
 
 		IConfigurationElement[] children = editorMenu.getChildren();
 
@@ -72,44 +55,56 @@ public class LanguageActions implements ILanguageActionsContributor {
 		return actions;
 	}
 
-	private Set<Action> createActionsForEditorMenuItem(
+	private List<Action> createActionsForEditorMenuItem(
 			IConfigurationElement editorMenuItem, String menuPath,
 			final UniversalEditor editor) {
 
-		Set<Action> actions = new HashSet<Action>();
-		String label = editorMenuItem.getAttribute("name");
+		List<Action> actions = new ArrayList<Action>();
+		String name = editorMenuItem.getAttribute("name");
 
 		if (!menuPath.equals("")) {
-			label = menuPath + "->" + label;
+			menuPath += "->" + name;
+		}
+		else {
+			menuPath += name;
 		}
 
 		if (editorMenuItem.getName().equals("EditorSubmenu")) {
 			IConfigurationElement[] children = editorMenuItem.getChildren();
 
 			for (IConfigurationElement child : children) {
-				actions.addAll(createActionsForEditorMenuItem(child, label,
+				actions.addAll(createActionsForEditorMenuItem(child, menuPath,
 						editor));
 			}
 		}
 
 		else if (editorMenuItem.getName().equals("EditorMenuAction")) {
-			final String toolbus_action = editorMenuItem
-					.getAttribute("toolbus_action");
-
-			Action action = new Action(label) {
-				public void run() {
-					Activator.getInstance().getTheLanguageActionsTool()
-							.PerformAction(toolbus_action, getFileName(editor));
-				}
-			};
-			action.setToolTipText(editorMenuItem.getAttribute("description"));
-			actions.add(action);
+			actions.add(createActionForEditorMenuAction(editorMenuItem,
+					menuPath, editor));
 		}
 
 		return actions;
 	}
 
-	private String getFileName(UniversalEditor editor) {
+	private Action createActionForEditorMenuAction(
+			IConfigurationElement editorMenuAction, final String menuPath,
+			final UniversalEditor editor) {
+		final String toolbus_action = editorMenuAction
+				.getAttribute("toolbus_action");
+		final String language = editor.fLanguage.getName();
+
+		Action action = new Action(menuPath) {
+			public void run() {
+				Activator.getInstance().getTheLanguageActionsTool()
+						.PerformAction(toolbus_action, language, getFileName(editor));
+			}
+		};
+		action.setToolTipText(editorMenuAction.getAttribute("description"));
+
+		return action;
+	}
+
+	public static String getFileName(UniversalEditor editor) {
 		IParseController controller = editor.getParseController();
 		ISourceProject project = controller.getProject();
 		IPath path = controller.getPath();
@@ -127,12 +122,12 @@ public class LanguageActions implements ILanguageActionsContributor {
 		return filename;
 	}
 
-	public static Set<IConfigurationElement> getExtensionsForLanguage(
+	public static List<IConfigurationElement> getExtensionsForLanguage(
 			IExtensionPoint extensionPoint, Language language) {
 
 		IConfigurationElement[] elements = extensionPoint
 				.getConfigurationElements();
-		Set<IConfigurationElement> result = new HashSet<IConfigurationElement>();
+		List<IConfigurationElement> result = new ArrayList<IConfigurationElement>();
 
 		if (elements != null) {
 			for (int n = 0; n < elements.length; n++) {
