f6fd809eb008b4624a43da0621e9414685d9c41e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-06-03 14:05:37 +0000

    Initial import of a prototype ToolBus IDE

---
+++ toolbus-ide/src/toolbus_ide/Activator.java
@@ -0,0 +1,73 @@
+package toolbus_ide;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.imp.preferences.PreferencesService;
+import org.eclipse.imp.runtime.PluginBase;
+import org.osgi.framework.BundleContext;
+
+/*
+ * SMS 27 Mar 2007:  Deleted creation of preferences cache (now obsolete)
+ * SMS 28 Mar 2007:
+ * 	- Plugin class name now totally parameterized
+ *  - Plugin package made a separate parameter
+ * SMS 19 Jun 2007:
+ * 	- Added kLanguageName (may be used by later updates to the template)
+ * 	- Added field and method related to new preferences service; deleted
+ *	  code for initializing preference store from start(..) method
+ */
+
+public class Activator extends PluginBase {
+
+	public static final String kPluginID = "toolbus_ide";
+
+	public static final String kLanguageName = "tscript";
+
+	/**
+	 * The unique instance of this plugin class
+	 */
+	protected static Activator sPlugin;
+
+	public static Activator getInstance() {
+		// SMS 11 Jul 2007
+		// Added conditional call to constructor in case the plugin
+		// class has not been auto-started
+		if (sPlugin == null)
+			new Activator();
+		return sPlugin;
+	}
+
+	public Activator() {
+		super();
+		sPlugin = this;
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+
+	}
+
+	public String getID() {
+		return kPluginID;
+	}
+
+	protected static PreferencesService preferencesService = null;
+
+	public static PreferencesService getPreferencesService() {
+		if (preferencesService == null) {
+			preferencesService = new PreferencesService(ResourcesPlugin
+					.getWorkspace().getRoot().getProject());
+			preferencesService.setLanguageName(kLanguageName);
+			// To trigger the invocation of the preferences initializer:
+			try {
+				new DefaultScope().getNode(kPluginID);
+			} catch (Exception e) {
+				// If this ever happens, it will probably be because the preferences
+				// and their initializer haven't been defined yet.  In that situation
+				// there's not really anything to do--you can't initialize preferences
+				// that don't exist.  So swallow the exception and continue ...
+			}
+		}
+		return preferencesService;
+	}
+}
---
+++ toolbus-ide/src/toolbus_ide/parser/ParseController.java
@@ -0,0 +1,92 @@
+package toolbus_ide.parser;
+
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.imp.model.ISourceProject;
+import org.eclipse.imp.parser.IMessageHandler;
+import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.ISourcePositionLocator;
+import org.eclipse.imp.services.IAnnotationTypeInfo;
+import org.eclipse.imp.services.ILanguageSyntaxProperties;
+import org.eclipse.jface.text.IRegion;
+
+import toolbus.ToolBus;
+import toolbus.parsercup.parser.SyntaxErrorException;
+
+public class ParseController implements IParseController {
+
+	private IMessageHandler handler;
+	private IPath filePath;
+	private ISourceProject project;
+	private String absPath;
+	private ToolBus toolbus;
+
+	public IAnnotationTypeInfo getAnnotationTypeInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+    public Iterator getTokenIterator(IRegion region) {
+//    	toolbus.getTokenIterator(region);
+    	return null;
+    }
+
+	public Object getCurrentAst() {
+		return toolbus;
+	}
+
+	public ISourcePositionLocator getNodeLocator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IPath getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ISourceProject getProject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ILanguageSyntaxProperties getSyntaxProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
+		this.handler = handler;
+		this.filePath = filePath;
+		this.project = project;
+		
+//		 try to make path absolute
+		IFile file = project.getRawProject().getFile(filePath);
+		
+		if (file.exists()) {
+			absPath = file.getLocation().toOSString();
+		} else {
+			absPath = filePath.toOSString();
+		}
+	}
+
+	public Object parse(String input, boolean scanOnly, IProgressMonitor monitor) {
+		toolbus = new ToolBus(new String[] {"-S" + absPath, "-I."});
+		try {
+		  toolbus.parsecup();
+		  return toolbus;
+	    }
+	    catch (SyntaxErrorException e) { // parser
+		  System.err.print(e);	
+		}
+	    catch (Error e) { // scanner
+	       System.err.print(e);	
+	    }
+		
+		
+		return toolbus;
+	}
+}
