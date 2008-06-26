81d1f0dae70f479122728fadff6f509923e5a3d5
Magiel Bruntink <magiel.bruntink@gmail.com>
2008-06-26 15:21:32 +0000

    Implemented cancelling of jobs.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/jobs/Jobs.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/jobs/Jobs.java
@@ -9,10 +9,12 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.meta_environment.eclipse.Tool;
 
+import aterm.ATerm;
+
 public class Jobs extends Tool {
 	private static final String TOOL_NAME = "jobs-manager";
 
-	private Map<String, ToolBusJob> jobs = new HashMap<String,ToolBusJob>();
+	private Map<String, ToolBusJob> jobs = new HashMap<String, ToolBusJob>();
 
 	private static class InstanceKeeper {
 		private static Jobs sInstance = new Jobs();
@@ -32,40 +34,74 @@ public class Jobs extends Tool {
 	public String getName() {
 		return TOOL_NAME;
 	}
-	
-	public void startJob(String message) {
+
+	public void startJob(String message, ATerm aborter) {
 		ToolBusJob job = jobs.get(message);
-		
-		if (job == null) {
-		  job = new ToolBusJob(message);	
-		  job.schedule();
-		  jobs.put(message, job);
-		}
-		else {
+
+		if (true || job == null) {
+			job = new ToolBusJob(message, aborter);
+			job.schedule();
+			jobs.put(message, job);
+		} else {
 			job.schedule();
 		}
 	}
-	
+
 	public void endJob(String message) {
 		ToolBusJob job = jobs.get(message);
 		if (job != null) {
 			job.done(Status.OK_STATUS);
+			job.setJobDone(true);
 		}
 	}
-	
+
 	private class ToolBusJob extends Job {
-		public ToolBusJob(String name) {
+		ATerm aborter;
+
+		public ToolBusJob(String name, ATerm aborter) {
 			super(name);
+			this.aborter = aborter;
 		}
 
 		@Override
-		protected IStatus run(IProgressMonitor monitor) {
-			return ASYNC_FINISH;
+		public boolean equals(Object obj) {
+			if (obj instanceof ToolBusJob) {
+				return ((ToolBusJob) obj).getName().equals(this.getName());
+			}
+			return false;
 		}
-		
+
+		private Boolean jobDone = false;
+
+		protected synchronized void setJobDone(boolean done) {
+			jobDone = done;
+		}
+
+		protected synchronized boolean getJobDone() {
+			return jobDone;
+		}
+
 		@Override
-		protected void canceling() {
-			done(Status.CANCEL_STATUS);
+		protected IStatus run(final IProgressMonitor monitor) {
+
+			Thread monitorPoller = new Thread(getName() + "Thread") {
+				public void run() {
+					try {
+						while (!monitor.isCanceled() && !getJobDone()) {
+							Thread.sleep(1000);
+						}
+						if (monitor.isCanceled()) {
+							sendEvent(factory
+									.make("abort-job(<term>)", aborter));
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+
+			monitorPoller.start();
+			return ASYNC_FINISH;
 		}
 	}
 }
