3dc8f806b9872a405ebd8842109634f93cf2184d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2008-07-01 13:52:36 +0000

    fiddled around with builder, but it still does not work

--- toolbus-ide/src/toolbus_ide/ToolBusNature.java
+++ toolbus-ide/src/toolbus_ide/ToolBusNature.java
@@ -1,18 +1,15 @@
 package toolbus_ide;
 
 import org.eclipse.core.resources.IProject;
-
 import org.eclipse.imp.builder.ProjectNatureBase;
 import org.eclipse.imp.runtime.IPluginLog;
 
-import org.eclipse.imp.smapifier.builder.SmapiProjectNature;
-
-import toolbus_ide.Activator;
-
 public class ToolBusNature extends ProjectNatureBase {
-	// SMS 28 Mar 2007:  plugin class now totally parameterized
-	public static final String k_natureID = Activator.kPluginID + ".imp.nature";
+	public static final String k_natureID = Activator.kPluginID + ".nature";
 
+	public ToolBusNature() {
+		// TODO Auto-generated constructor stub
+	}
 	public String getNatureID() {
 		return k_natureID;
 	}
@@ -23,15 +20,12 @@ public class ToolBusNature extends ProjectNatureBase {
 
 	public void addToProject(IProject project) {
 		super.addToProject(project);
-		new SmapiProjectNature("tscript").addToProject(project);
 	};
 
 	protected void refreshPrefs() {
-		// TODO implement preferences and hook in here
 	}
 
 	public IPluginLog getLog() {
-		// SMS 28 Mar 2007:  plugin class now totally parameterized
 		return Activator.getInstance();
 	}
 
