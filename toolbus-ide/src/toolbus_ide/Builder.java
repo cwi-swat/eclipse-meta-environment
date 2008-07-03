96504cf8d9f37045f21e2305f805858476165277
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-07-03 12:37:22 +0000

    -Added dead code analysis (it may cause duplicate markers at the moment if multiple files in the same hierarchy are being edited/build).

--- toolbus-ide/src/toolbus_ide/Builder.java
+++ toolbus-ide/src/toolbus_ide/Builder.java
@@ -3,6 +3,8 @@ package toolbus_ide;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
@@ -14,13 +16,22 @@ import org.eclipse.imp.language.Language;
 import org.eclipse.imp.language.LanguageRegistry;
 import org.eclipse.imp.runtime.PluginBase;
 
+import toolbus.AtomSet;
 import toolbus.ToolBus;
+import toolbus.atom.Atom;
+import toolbus.atom.msg.RecMsg;
+import toolbus.atom.msg.SndMsg;
+import toolbus.atom.note.SndNote;
+import toolbus.atom.note.Subscribe;
 import toolbus.exceptions.SyntaxErrorException;
 import toolbus.exceptions.ToolBusException;
 import toolbus.exceptions.ToolBusExecutionException;
+import toolbus.matching.MatchStore;
 import toolbus.parsercup.PositionInformation;
 import toolbus.parsercup.parser;
 import toolbus.parsercup.parser.UndeclaredVariableException;
+import toolbus.process.ProcessDefinition;
+import toolbus.process.ProcessExpression;
 import toolbus_ide.editor.ParseController;
 import aterm.ATerm;
 
@@ -79,19 +90,73 @@ public class Builder extends BuilderBase {
 		try{
 			parser parser_obj = new parser(new HashSet<String>(), new ArrayList<ATerm>(), filename, new FileReader(filename), toolbus);
 			parser_obj.parse();
+			
+			findDeadCommunicationAtoms(file, toolbus);
 		}catch(SyntaxErrorException see){ // Parser.
-			ErrorHandler.addMarker(file, see.position, see.line, see.column, "Syntax error");
+			ErrorHandler.addProblemMarker(file, see.position, see.line, see.column, "Syntax error");
 		}catch(UndeclaredVariableException uvex){ // Parser.
-			ErrorHandler.addMarker(file, uvex.position, uvex.line, uvex.column, "Undeclared variable");
+			ErrorHandler.addProblemMarker(file, uvex.position, uvex.line, uvex.column, "Undeclared variable");
 		}catch(ToolBusExecutionException e){
 			PositionInformation p = e.getPositionInformation();
-			ErrorHandler.addMarker(file, p.getOffset(), 0, 0, e.getMessage());
+			ErrorHandler.addProblemMarker(file, p.getOffset(), 0, 0, e.getMessage());
 		}catch(ToolBusException e){
-			ErrorHandler.addMarker(file, 0, 0, 0, e.getMessage());
+			ErrorHandler.addProblemMarker(file, 0, 0, 0, e.getMessage());
 		}catch(Error e){ // Scanner.
-			ErrorHandler.addMarker(file, 0, 0, 0, e.getMessage());
+			ErrorHandler.addProblemMarker(file, 0, 0, 0, e.getMessage());
 		}catch(Exception ex){ // Something else.
-			ErrorHandler.addMarker(file, 0, 0, 0, ex.getMessage());
+			ErrorHandler.addProblemMarker(file, 0, 0, 0, ex.getMessage());
+		}
+    }
+    
+    private void findDeadCommunicationAtoms(IFile file, ToolBus toolbus){
+    	final List<Atom> atomSignature = new ArrayList<Atom>();
+		List<ProcessDefinition> processDefinitions = toolbus.getProcessDefinitions();
+		Iterator<ProcessDefinition> processDefinitionsIterator = processDefinitions.iterator();
+		while(processDefinitionsIterator.hasNext()){
+			ProcessDefinition processDefinition = processDefinitionsIterator.next();
+			
+			ProcessExpression originalProcessExpression = processDefinition.getOriginalProcessExpression();
+			AtomSet atoms = originalProcessExpression.getAtoms();
+			Iterator<Atom> atomSetIterator = atoms.iterator();
+			while(atomSetIterator.hasNext()){
+				Atom a = atomSetIterator.next();
+				atomSignature.add(a);
+			}
+		}
+		
+		MatchStore matchStore = new MatchStore(toolbus.getTBTermFactory());
+		matchStore.initialize(atomSignature);
+		
+		List<RecMsg> partnerlessReceiveMessages = matchStore.findPartnerLessReceiveMessageAtoms();
+		Iterator<RecMsg> partnerlessReceiveMessagesIterator = partnerlessReceiveMessages.iterator();
+		while(partnerlessReceiveMessagesIterator.hasNext()){
+			RecMsg recMsg = partnerlessReceiveMessagesIterator.next();
+			PositionInformation posInfo = recMsg.getPosInfo();
+			ErrorHandler.addWarningMarker(file, 0, posInfo.getBeginLine(), posInfo.getBeginColumn(), "Potentially partnerless communication atom");
+		}
+		
+		List<SndMsg> partnerlessSendMessages = matchStore.findPartnerlessSendMessageAtoms();
+		Iterator<SndMsg> partnerlessSendMessagesIterator = partnerlessSendMessages.iterator();
+		while(partnerlessSendMessagesIterator.hasNext()){
+			SndMsg sndMsg = partnerlessSendMessagesIterator.next();
+			PositionInformation posInfo = sndMsg.getPosInfo();
+			ErrorHandler.addWarningMarker(file, 0, posInfo.getBeginLine(), posInfo.getBeginColumn(), "Potentially partnerless communication atom");
+		}
+		
+		List<SndNote> partnerlessSendNotes = matchStore.findPartnerlessSendNoteAtoms();
+		Iterator<SndNote> partnerlessSendNotesIterator = partnerlessSendNotes.iterator();
+		while(partnerlessSendNotesIterator.hasNext()){
+			SndNote sndNote = partnerlessSendNotesIterator.next();
+			PositionInformation posInfo = sndNote.getPosInfo();
+			ErrorHandler.addWarningMarker(file, 0, posInfo.getBeginLine(), posInfo.getBeginColumn(), "Potentially partnerless communication atom");
+		}
+		
+		List<Subscribe> partnerlessSubscribes = matchStore.findPartnerlessSubscribeAtoms();
+		Iterator<Subscribe> partnerlessSubscribesIterator = partnerlessSubscribes.iterator();
+		while(partnerlessSubscribesIterator.hasNext()){
+			Subscribe subscribe = partnerlessSubscribesIterator.next();
+			PositionInformation posInfo = subscribe.getPosInfo();
+			ErrorHandler.addWarningMarker(file, 0, posInfo.getBeginLine(), posInfo.getBeginColumn(), "Potentially partnerless communication atom");
 		}
     }
     
--- toolbus-ide/src/toolbus_ide/ErrorHandler.java
+++ toolbus-ide/src/toolbus_ide/ErrorHandler.java
@@ -10,8 +10,16 @@ public class ErrorHandler{
 	private ErrorHandler(){
 		super();
 	}
+
+	public static void clearMarkers(IFile file) {
+		try{
+    		file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+	    }catch(CoreException ce){
+			// I don't care what happened, just ignore this.
+		}
+	}
 	
-	public static void addMarker(IFile file, int charOffset, int line, int column, String message){
+	public static void addProblemMarker(IFile file, int charOffset, int line, int column, String message){
     	try{
 	    	IMarker m = file.createMarker(IMarker.PROBLEM);
 	
@@ -26,12 +34,20 @@ public class ErrorHandler{
     		// I don't care what happened, just ignore this.
     	}
     }
-
-	public static void clearMarkers(IFile file) {
-		try{
-    		file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-	    }catch(CoreException ce){
-			// I don't care what happened, just ignore this.
-		}
-	}
+	
+	public static void addWarningMarker(IFile file, int charOffset, int line, int column, String message){
+    	try{
+	    	IMarker m = file.createMarker(IMarker.PROBLEM);
+	
+			m.setAttribute(IMarker.TRANSIENT, true);
+			m.setAttribute(IMarker.CHAR_START, charOffset);
+			m.setAttribute(IMarker.CHAR_END, charOffset);
+			m.setAttribute(IMarker.MESSAGE, message);
+			m.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+			m.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+			m.setAttribute(IMarker.LOCATION, "line: " + line + ", column: " + column);
+    	}catch(CoreException ce){
+    		// I don't care what happened, just ignore this.
+    	}
+    }
 }
