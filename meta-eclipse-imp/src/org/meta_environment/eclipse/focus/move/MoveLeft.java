b0ba334d2630c4f3f5d677b775de7b225b6c80c7
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-10-02 09:12:12 +0000

    -Fixed some Eclipse warnings.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/focus/move/MoveLeft.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/focus/move/MoveLeft.java
@@ -2,19 +2,28 @@ package org.meta_environment.eclipse.focus.move;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-public class MoveLeft extends AbstractMove  implements IActionDelegate{
+public class MoveLeft extends AbstractMove implements IWorkbenchWindowActionDelegate{
 	
 	public MoveLeft(){
 		super();
 	}
 	
+	public void run(IAction action){
+		move("move-left(<term>, <str>, <int>, <int>, <int>, <int>)");
+	}
+	
 	public void selectionChanged(IAction action, ISelection selection){
 		// Ignore this.
 	}
+
+	public void init(IWorkbenchWindow window){
+		// Do nothing
+	}
 	
-	public void run(IAction action){
-		move("move-left(<term>, <str>, <int>, <int>, <int>, <int>)");
+	public void dispose(){
+		// Do nothing
 	}
 }
