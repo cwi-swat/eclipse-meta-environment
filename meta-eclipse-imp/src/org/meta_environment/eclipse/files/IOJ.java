d2fce31d18452d1233b129d05a9f0605ecf493f8
Arnold Lankamp <arnold.lankamp@gmail.com>
2008-10-10 14:22:42 +0000

    -Removed useless import.

--- meta-eclipse-imp/src/org/meta_environment/eclipse/files/IOJ.java
+++ meta-eclipse-imp/src/org/meta_environment/eclipse/files/IOJ.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import nl.cwi.sen1.configapi.types.Property;
