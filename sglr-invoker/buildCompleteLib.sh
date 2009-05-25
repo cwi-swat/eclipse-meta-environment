d268e991f8c130e38687beb32502adc3ac010991
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-05-25 13:11:55 +0000

    -CompleteLib is being build now.

---
+++ sglr-invoker/buildCompleteLib.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Find all .a files located in dependencies.
+ALL_ARCHIVES=""
+for dep in ${ALL_DEPS}; do
+   prefix=`pkg-config --variable prefix ${dep}`
+   for library in `find ${PREFIX}/lib/lib*.a`; do
+      if [ "`echo '${ALL_ARCHIVES}' | grep ${library}`" == "" ]; then
+         ALL_ARCHIVES="${ALL_ARCHIVES} ${library}"
+      fi
+   done
+done
+
+# Extract all .o files from the .a files. 
+mkdir -p ${TOP_DIR}/libbuild
+cd ${TOP_DIR}/libbuild
+for archive in ${ALL_ARCHIVES}; do 
+   ar -x ${archive}
+done
+cd -
+ALL_OBJECTS="`find ../libbuild/*.o`"
+
+echo $ALL_OBJECTS
