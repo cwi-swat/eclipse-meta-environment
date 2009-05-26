92a43aacc4b3a2960f54c63c13db8781a309151b
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-05-26 13:46:40 +0000

    -Please let it work this time ....

--- sglr-invoker/buildCompleteLib.sh
+++ sglr-invoker/buildCompleteLib.sh
@@ -18,6 +18,6 @@ for archive in ${ALL_ARCHIVES}; do
    ar -x ${archive}
 done
 cd -
-ALL_OBJECTS="`find ../libbuild/*.o`"
+ALL_OBJECTS="`find ../libbuild/*.o | grep -v SGLRInvoker`"
 
 echo $ALL_OBJECTS
