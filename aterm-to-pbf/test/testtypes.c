a980cd6179ad56d3e80950e36bc00960dbbf84a1
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-08-18 12:47:35 +0000

    -Added int size check. -Added Makefiles and stuff for tests. -Fixed lib/Makefile.am

---
+++ aterm-to-pbf/test/testtypes.c
@@ -0,0 +1,14 @@
+#include <pdbtypes.h>
+#include <aterm2.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	ATerm bottomOfStack;
+	ATinit(argc, argv, &bottomOfStack);
+	
+	/* TODO Implement. */
+	
+	return 0;
+}
+
