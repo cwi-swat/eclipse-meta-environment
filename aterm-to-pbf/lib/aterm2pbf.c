6e3d0f3fd631e00551b4ef6bbc6039d1110f247d
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-09-02 11:51:31 +0000

    -Cast.

--- aterm-to-pbf/lib/aterm2pbf.c
+++ aterm-to-pbf/lib/aterm2pbf.c
@@ -909,7 +909,7 @@ valueId = -1; /* Temp. */
 			writeDouble(writer, (ATermReal) value);
 			break;
 		case PDB_STRING_TYPE_HEADER:
-			if(ATgetType(value) != AT_APPL || ATisQuoted(ATgetAFun(value)) == ATfalse){ fprintf(stderr, "String didn't have 'quoted' AT_APPL type.\n"); ATabort(""); exit(1); }
+			if(ATgetType(value) != AT_APPL || ATisQuoted(ATgetAFun((ATermAppl) value)) == ATfalse){ fprintf(stderr, "String didn't have 'quoted' AT_APPL type.\n"); ATabort(""); exit(1); }
 			writeString(writer, (ATermAppl) value);
 			break;
 		case PDB_SOURCE_LOCATION_TYPE_HEADER:
